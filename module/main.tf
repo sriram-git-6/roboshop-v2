1ec539e2bfd6ad88c60379aab61266041d6c129f
sriram <sriram.mudunuru@gmail.com>
2023-08-20 20:29:54 +0530

    terraform

--- module/main.tf
+++ module/main.tf
@@ -15,3 +15,18 @@ resource "aws_route53_record" "record" {
   ttl     = 30
   records = [aws_instance.instance.private_ip]
 }
+
+resource "null_resource" "ansible" {
+  depends_on = [
+  aws_route53_record.record
+  ]
+  provisioner "local-exec" {
+    command = <<EOF
+cd /home/centos/ansible-roboshop-roles
+git pull
+sleep 30
+ansible-playbook -i ${var.name}-dev.devops746.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e
+component=${var.name}
+EOF
+  }
+}
